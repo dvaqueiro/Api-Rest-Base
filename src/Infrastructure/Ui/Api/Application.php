3e864e9413b87c195241a735eed78fe2b72be742
dvaqueiro <dvaqueiro@192.168.0.12>
2017-05-20 17:35:32 +0200

    add X-AUTH-TOKEN security

--- src/Infrastructure/Ui/Api/Application.php
+++ src/Infrastructure/Ui/Api/Application.php
@@ -2,6 +2,8 @@
 
 namespace dvaqueiro\Infrastructure\Ui\Api;
 
+use Silex\Provider\SecurityServiceProvider;
+
 /**
  * Description of Application
  *
@@ -14,6 +16,26 @@ class Application
         $app = new \Silex\Application();
 
         $app['debug'] = true;
+
+        $app['app.token_authenticator'] = function ($app) {
+            return new TokenAuthenticator($app['security.encoder_factory']);
+        };
+
+        $app['security.firewalls'] = array(
+            'main' => array(
+                'guard' => array(
+                    'authenticators' => array(
+                        'app.token_authenticator'
+                    ),
+                ),
+                'users' => array(
+                    //raw password = foo
+                    'dvaqueiro' => array('ROLE_USER', '$2y$10$3i9/lVd8UOFIJ6PAMFt8gu3/r5g0qeCJvoSlLCsvMTythye19F77a'),
+                ),
+            ),
+        );
+
+        $app->register(new SecurityServiceProvider(), $app['security.firewalls']);
 
         return $app;
     }
